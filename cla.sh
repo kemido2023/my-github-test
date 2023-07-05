ce4d7e3ee4223e928a53ffd8e74e7386cacd0e3d
kemido2023 <kemido2021@gmail.com>
2023-07-05 10:16:22 +0100

    argument test

---
+++ cla.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+date
+whoami
+echo 'The scriptName is' $0
+echo 'The first argument is' $1
+echo 'The 10th argument is' ${10}
+echo 'The processID is'    $$
+echo 'The arguments are' $*
+echo 'The number arguments is' $@
+date 
+echo "The status of the result last command is " $?
+
