f9135c0a39726f296e07574347434e9037ed696e
kemido2023 <kemido2021@gmail.com>
2023-07-06 09:36:18 +0100

    additional writeups

--- debug.sh
+++ debug.sh
@@ -2,4 +2,8 @@
   echo 'Welcome to this beautiful morning'
   date
   echo 'You have login as' `whoami`
-  pwd 
+  pwd
+ $0
+echo "Mr $name it is another beautiful day to get better in coding"
+name=KESIYE
+echo "cheers"
